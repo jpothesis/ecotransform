40a8c585c9f2027b211622f1b9b0765a0dede30c
jpothesis <105847313+jpothesis@users.noreply.github.com>
2025-11-15 16:35:50 +0530

    forder corrected 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 
-import Navbar from "./components/Navbar";
+import Navbar from "./components/navbar";
 import HeroSection from "./components/hero-section";
 import FeatureSection from "./components/features-section";
 import CommunitySection from "./components/community-section";
